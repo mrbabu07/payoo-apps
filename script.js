ae476417d76f471edf8b9e9a3179235a8cf7bcf7
md jahedul islam <mdjahedulislamjaved@gmail.com>
2025-08-25 20:57:21 +0600

    added add money option

--- script.js
+++ script.js
@@ -2,6 +2,24 @@
 document.getElementById('loginButton')
 .addEventListener('click', function(event){
     event.preventDefault()
-    console.log('login button clicked')
+    
+    const mobileNumber = 1234567890;
+    const pinNumber = 1234;
+
+    const mobileNumberValue = document.getElementById("mobile-number").value 
+    const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+    const pinNumberValue = document.getElementById("pin-number").value
+    const pinNumberValueConverted = parseInt(pinNumberValue);
+
+    console.log(mobileNumberValueConverted,pinNumberValueConverted);
+
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+        window.location.href="./home.html"
+    }
+        else {
+            alert("invalid credentials");
+        }
+    
 
 })
