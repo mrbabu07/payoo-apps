2623a09bc9f76c1fe9f1ac5a95b7765b3b16409a
md jahedul islam <mdjahedulislamjaved@gmail.com>
2025-08-25 17:30:54 +0600

    added the login page with clickable eventlistiner

---
+++ script.js
@@ -0,0 +1,7 @@
+// login button functionallity
+document.getElementById('loginButton')
+.addEventListener('click', function(event){
+    event.preventDefault()
+    console.log('login button clicked')
+
+})
