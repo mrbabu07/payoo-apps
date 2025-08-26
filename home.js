451009ad38c7d323818f29507b50a8e957436916
md jahedul islam <mdjahedulislamjaved@gmail.com>
2025-08-26 19:32:34 +0600

    added cashout section successfully

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+const validPin = 1234;
+// add money feature 
 document.getElementById("add-money-btn")
 .addEventListener('click', function(event){
     event.preventDefault()
@@ -7,7 +9,17 @@ document.getElementById("add-money-btn")
     const accountNumber = document.getElementById("account-number").value
 
     const amount = parseInt (document.getElementById("add-amount").value)
-    const pin = document.getElementById("add-pin").value
+    const pin = parseInt (document.getElementById("add-pin").value)
+
+    if(accountNumber.length < 11 ){
+        alert('Please Provide Valid Account Number')
+        return;
+    }
+
+    if(pin !== validPin){
+        alert('please provide valid pin number')
+        return;
+    }
 
     const availableBalance = parseInt (document.getElementById("available-balance").innerText)
     console.log(availableBalance);
@@ -15,5 +27,49 @@ document.getElementById("add-money-btn")
     const totalNewAvailableBalance = amount + availableBalance;
 
     document.getElementById("available-balance").innerText = totalNewAvailableBalance;
+
+})
+
+// cash out money feature 
+
+
+document.getElementById("withdraw-btn")
+.addEventListener("click", function(event){
+    event.preventDefault()
+
+    const agentnumber = document.getElementById("agent-number").value
+    const pin = parseInt (document.getElementById("withdraw-pin").value)
+    
+     if(agentnumber.length < 11 ){
+        alert('Please Provide Valid  Number')
+        return;
+    }
+
+    if(pin !== validPin){
+        alert('please provide valid pin number')
+        return;
+    }
+
+    const amount = parseInt(document.getElementById("withdraw-amount").value)
+    const availableBalance = parseInt (document.getElementById("available-balance").innerText)
+    
+    const totalNewAvailableBalanceavailableBalance = availableBalance - amount
+    console.log(totalNewAvailableBalanceavailableBalance)
+
+    document.getElementById("available-balance").innerText = totalNewAvailableBalanceavailableBalance; 
+
+})
+
+// toggle feature 
+document.getElementById("add-btn")
+.addEventListener("click", function(){
+document.getElementById("cash-out-parent").style.display = "none"
+document.getElementById("add-money-parent").style.display = "block"
+})
+
+document.getElementById("cash-out-btn")
+.addEventListener("click", function(){
+document.getElementById("add-money-parent").style.display = "none"
+document.getElementById("cash-out-parent").style.display = "block"
 
 })
